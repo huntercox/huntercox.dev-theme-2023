f39b8aa79efeb3012d825fc108cdbcf016db3fae
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-05-05 00:35:05 -0400

    Added more employer info to each item on the resume page.

--- parts/post-employer.php
+++ parts/post-employer.php
@@ -4,46 +4,12 @@ if (hsc_is_current_employer($post->ID)) {
 	$classes .= ' employer--current';
 }
 $classes .= ' employer--' . $post->post_name;
+?>
+<div class="employer<?php echo esc_attr($classes); ?>">
+	<h2 class="employer__name"><?php echo $post->post_title ?></h2>
+	<div class="employer__info">
+		<h5 class="employer__job-title"><?php the_field('job_title', $post->ID); ?></h5>
+		<div class="employer__job-description"><?php the_content(); ?></div>
+	</div>
 
-echo '<div class="employer' . esc_attr($classes) . '">';
-
-/* Employer Name */
-$employer_name = $post->post_title;
-echo '<h2 class="employer__name">' . $employer_name . '</h2>';
-
-$employment_dates = get_field('employment_dates', $post->ID);
-$employment_start_date = $employment_dates['start_date'];
-$employment_end_date = '';
-
-echo '<div class="employer__dates">';
-
-// Start & End Date
-if ($employment_start_date) {
-	echo '<div class="employer__dates__start-date">';
-	echo 'Start Date: ' . $employment_start_date;
-	echo '</div>';
-}
-
-if (!hsc_is_current_employer($post->ID)) {
-	$employment_end_date = $employment_dates['end_date'];
-
-	if ($employment_end_date) {
-		echo '<div class="employer__dates__end-date">';
-		echo 'End Date: ' . $employment_end_date;
-		echo '</div>';
-	}
-}
-
-// Length of Time Employed
-if ($employment_start_date && $employment_end_date) {
-	$employment_dates = array(
-		'start_date' => $employment_start_date,
-		'end_date' => $employment_end_date
-	);
-	$length_of_employment = hsc_get_length_of_employment($employment_dates);
-	echo $length_of_employment;
-}
-
-echo '</div>';
-
-echo '</div>';
+</div>
