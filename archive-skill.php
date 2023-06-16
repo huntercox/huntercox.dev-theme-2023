049f26579e9a028a6d82a8acbb07a7062ae26e9c
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-16 16:15:54 -0400

    Remove wp-sqlite from composer.json file. Made changes to the frontend of the Skills archive page.

--- archive-skill.php
+++ archive-skill.php
@@ -4,33 +4,40 @@ get_header();
 
 <div class="page__content">
 	<div class="container">
-		<?php
-		if (have_posts()) :
-			echo '<ul class="skill__list">';
+		<div class="skill-archive-container">
+			<?php
+			$color_counter = 0;
+			$colors = hsc_get_colors();
 
-			$count = $wp_query->found_posts;
-			$current = 1;
+			if (have_posts()) : ?>
 
-			while (have_posts()) :
-				the_post();
-		?>
-				<li class="skill">
-					<?php
-					if ($current < $count) {
-						echo '<h4><a href="' . get_the_permalink() . '">' . get_the_title() . '</a>, &nbsp; </h4>';
-					} else {
-						echo '<h4><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h4>';
-					}
+				<ul class="skills label-list">
+					<?php while (have_posts()) : the_post();
+						$color = get_field('color', $post->ID);
+
+						if (!$color) {
+							$color_keys = array_keys($colors);
+							$color_key = $color_keys[$color_counter % count($color_keys)];
+							$color = $colors[$color_key];
+							$color_counter++;
+						}
+
+						// Check the background color and set the text color accordingly
+						$text_color = '#15023d';  // Default text color
+						if ($color == $colors['purple'] || $color == $colors['red']) {
+							$text_color = '#ffffff';  // Change text color for purple and red backgrounds
+						}
 					?>
-				</li>
-		<?php
-
-				$current++;
-
-			endwhile;
-			echo '</ul>';
-
-		endif; ?>
+						<li class="skill" style="background-color: <?php echo $color; ?>;">
+							<a href="<?php the_permalink(); ?>" style="color: <?php echo $text_color; ?>;">
+								<?php the_title(); ?>
+							</a>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+			<?php endif;
+			?>
+		</div>
 	</div>
 </div>
 
