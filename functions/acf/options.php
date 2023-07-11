06e9eb9f733944ea90f9f905bb6ab58f6a41bfb9
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-10 20:15:20 -0400

    Remove unused options pages from WP admin.

--- functions/acf/options.php
+++ functions/acf/options.php
@@ -1,10 +1,5 @@
 <?php
 if (function_exists('acf_add_options_page')) {
 
-	acf_add_options_page();
-
-	// Add Dev Skills options page
-	acf_add_options_page(
-		'Dev Skills'
-	);
+	// acf_add_options_page();
 }
